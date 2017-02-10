8b5c4ea9c4005badc39e12f171458a4fcc0f8834
203p01 <203p01@203p24-PC>
2017-02-10 09:45:39 +0100

    Segundo punto

--- src/fecha/Fecha.java
+++ src/fecha/Fecha.java
@@ -8,4 +8,11 @@ public class Fecha {
 
     int dia, mes, anio;
 
+    public Fecha(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+    
+ 
 }
