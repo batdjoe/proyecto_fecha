9c974a152cc42b53b08ef76fbda25593103e5820
203p01 <203p01@203p24-PC>
2017-02-10 09:54:24 +0100

    Tercer punto

--- src/fecha/Fecha.java
+++ src/fecha/Fecha.java
@@ -1,5 +1,8 @@
 package fecha;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author Daniel Marrodán
@@ -8,11 +11,17 @@ public class Fecha {
 
     int dia, mes, anio;
 
-    public Fecha(int dia, int mes, int anio) {
-        this.dia = dia;
+    public Fecha(int dias, int mes, int anio) {
+        this.dia = dias;
         this.mes = mes;
         this.anio = anio;
     }
-    
- 
+
+    public Fecha() {
+        Calendar fechaHoy = new GregorianCalendar();
+        dia = fechaHoy.get(Calendar.DAY_OF_MONTH);
+        mes = fechaHoy.get(Calendar.MONTH) + 1;
+        anio = fechaHoy.get(Calendar.YEAR);
+        
+    }
 }
